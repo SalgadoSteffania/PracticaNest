edef931c95aec99fa6d5333b6fce65b644ed73e4
Steffania Salgado <stefani28sg@gmail.com>
2025-10-11 09:39:18 -0600

    Commit segunda guia

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
+  exports: [PrismaService],
+  imports: [PrismaModule, UsersModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const config =new DocumentBuilder()
+  .setTitle('My API')
+  .setDescription('API description')
+  .setVersion('1.0')
+  .addTag('api')
+  .build();
+
+  
+  const document = SwaggerModule.createDocument(app, config);
+ SwaggerModule.setup('api', app, document); // <— ruta: /api
+
   await app.listen(process.env.PORT ?? 3000);
+  //await app.listen(3001);
 }
 bootstrap();
